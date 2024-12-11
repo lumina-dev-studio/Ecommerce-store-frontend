d5e1e7ffddbad2172cedfe5f149601e1e21b29dc
Lumina Dev Studio <luminadevstudio@gmail.com>
2024-12-11 14:26:17 +0600

    fixed container

--- src/SharedComponent/Container/Container.tsx
+++ src/SharedComponent/Container/Container.tsx
@@ -1,13 +1,8 @@
-const Container = ({children}:any) => {
-  return (
-    <div className="w-[90%] mx-auto 4xl:mx-auto md:me-10 4xl:w-[70%]  3xl:pe-10">
-    {/* <div className="  container "> */}
-      
-    {children}
-  </div>
-  );
+const Container = ({children}: any) => {
+    return (
+        <div className="container mx-auto">
+            {children}
+        </div>
+    );
 };
-
 export default Container;
-
-// tonu
